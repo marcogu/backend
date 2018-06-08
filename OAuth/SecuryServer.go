ac406923c7b92793789b96ca0370a4c96e5f44eb
YingJiong.Gu <penpenqie>
2018-06-08 10:35:42 +0800

    dev auth server

---
+++ OAuth/SecuryServer.go
@@ -0,0 +1,106 @@
+package OAuth
+
+import (
+	"fmt"
+	"github.com/RangelReale/osin"
+	"github.com/RangelReale/osin/example"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"net/url"
+)
+
+type GinAuthServer struct {
+	osinServer *osin.Server
+}
+
+func GetDefaultAuthServer() *GinAuthServer {
+	fackeStorage := example.NewTestStorage()
+	s := osin.NewServer(NewDefaultAuthServiceCfg(), fackeStorage)
+	authServer := &GinAuthServer{}
+	authServer.osinServer = s
+	return authServer
+}
+
+func NewDefaultAuthServiceCfg() *osin.ServerConfig {
+	sconfig := osin.NewServerConfig()
+	sconfig.AllowedAuthorizeTypes = osin.AllowedAuthorizeType{osin.CODE, osin.TOKEN}
+	sconfig.AllowedAccessTypes = osin.AllowedAccessType{osin.AUTHORIZATION_CODE,
+		osin.REFRESH_TOKEN, osin.PASSWORD, osin.CLIENT_CREDENTIALS, osin.ASSERTION}
+	sconfig.AllowGetAccessRequest = true
+	sconfig.AllowClientSecretInParams = true
+	return sconfig
+}
+
+func (s *GinAuthServer) AuthorizeReqHandler(c *gin.Context) {
+	resp := s.osinServer.NewResponse()
+	defer resp.Close()
+
+	if ar := s.osinServer.HandleAuthorizeRequest(resp, c.Request); ar != nil {
+		if !example.HandleLoginPage(ar, c.Writer, c.Request) {
+			return
+		}
+		ar.UserData = struct{ Login string }{Login: "test"}
+		ar.Authorized = true
+		s.osinServer.FinishAuthorizeRequest(resp, c.Request, ar)
+	}
+	if resp.IsError && resp.InternalError != nil {
+		fmt.Printf("ERROR: %s\n", resp.InternalError)
+	}
+	if !resp.IsError {
+		resp.Output["custom_parameter"] = 187723
+	}
+	osin.OutputJSON(resp, c.Writer, c.Request)
+}
+
+func (s *GinAuthServer) AuthCodeReqHandler(c *gin.Context) {
+	code := c.Query("code")
+	c.Writer.Write([]byte("<html><body>"))
+	c.Writer.Write([]byte("APP AUTH - CODE<br/>"))
+	defer c.Writer.Write([]byte("</body></html>"))
+
+	if code == "" {
+		c.Writer.Write([]byte("Nothing to do"))
+		return
+	}
+	jr := make(map[string]interface{})
+	// build access code url
+	aurl := fmt.Sprintf("/token?grant_type=authorization_code&client_id=1234&state=xyz&redirect_uri=%s&code=%s",
+		url.QueryEscape("http://localhost:14000/appauth/code"), url.QueryEscape(code))
+
+	if c.Query("doparse") == "1" { // if parse, download and parse json
+		err := example.DownloadAccessToken(fmt.Sprintf("http://localhost:14000%s", aurl),
+			&osin.BasicAuth{"1234", "aabbccdd"}, jr)
+		if err != nil {
+			c.Writer.Write([]byte(err.Error()))
+			c.Writer.Write([]byte("<br/>"))
+		}
+	}
+
+	if erd, ok := jr["error"]; ok { // show json error
+		c.Writer.Write([]byte(fmt.Sprintf("ERROR: %s<br/>\n", erd)))
+	}
+
+	if at, ok := jr["access_token"]; ok { // show json access token
+		c.Writer.Write([]byte(fmt.Sprintf("ACCESS TOKEN: %s<br/>\n", at)))
+	}
+	c.Writer.Write([]byte(fmt.Sprintf("FULL RESULT: %+v<br/>\n", jr)))
+	// output links
+	c.Writer.Write([]byte(fmt.Sprintf("<a href=\"%s\">Goto Token URL</a><br/>", aurl)))
+
+	cururl := *c.Request.URL
+	curq := cururl.Query()
+	curq.Add("doparse", "1")
+	cururl.RawQuery = curq.Encode()
+	c.Writer.Write([]byte(fmt.Sprintf("<a href=\"%s\">Download Token</a><br/>", cururl.String())))
+}
+
+func (s *GinAuthServer) AuthClientIdxPageHandler(c *gin.Context) {
+	c.HTML(http.StatusOK, "authidx.tmpl", nil)
+}
+
+func (s *GinAuthServer) SetupGinRouter(router *gin.Engine) {
+	router.GET("authorize", s.AuthorizeReqHandler)
+	// Client Http API for receive the Auth Code from Authrize Server Redirect request.
+	router.GET("appauth/code", s.AuthCodeReqHandler)
+	router.GET("app", s.AuthClientIdxPageHandler)
+}
--- main.go
+++ main.go
@@ -1,13 +1,27 @@
 package main
 
 import (
+	"backend/OAuth"
 	"backend/github"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	bootWithAuthService()
+	//bootWithGithubClient()
+}
+
+func bootWithGithubClient() {
 	router := gin.Default()
 	router.LoadHTMLGlob("templates/*")
 	github.SetupGinRoute(router)
 	router.Run(":14002")
+}
+
+func bootWithAuthService() {
+	router := gin.Default()
+	router.LoadHTMLGlob("templates/*")
+	aserver := OAuth.GetDefaultAuthServer()
+	aserver.SetupGinRouter(router)
+	router.Run(":14001")
 }
