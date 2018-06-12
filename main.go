45c34925e5ad6806540f08f235ef7ac544287450
Scala Chen <3212108@qq.com>
2018-06-12 09:40:15 +0800

    Use environment variables to set database parameters.

--- main.go
+++ main.go
@@ -3,28 +3,68 @@ package main
 import (
 	"backend/OAuth"
 	"backend/github"
+	"database/sql"
+	"errors"
+	"fmt"
+	"github.com/felipeweb/osin-mysql"
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 	"github.com/prometheus/common/log"
-	"database/sql"
-	"github.com/felipeweb/osin-mysql"
+	"os"
 )
 
 type Server struct {
-	WebServer *gin.Engine
+	WebServer  *gin.Engine
 	AuthServer *OAuth.GinAuthServer
-	Db *gorm.DB
+	Db         *gorm.DB
 }
 
 func NewServer() (*Server, error) {
-	url := "user:password@tcp(host:3306)/dbname?parseTime=true"
-	db, err := sql.Open("mysql", url)
+	osinDbUsername, ok := os.LookupEnv("OSIN_DB_USERNAME")
+	if !ok || len(osinDbUsername) == 0 {
+		return nil, errors.New("No OSIN_DB_USERNAME env is provided!")
+	}
+
+	osinDbPassword, ok := os.LookupEnv("OSIN_DB_PASSWORD")
+	if !ok || len(osinDbPassword) == 0 {
+		log.Warn("No OSIN_DB_PASSWORD env is provided. If it is, your database server is NOT SECURE!")
+	}
+
+	osinDbHost, ok := os.LookupEnv("OSIN_DB_HOST")
+	if !ok || len(osinDbHost) == 0 {
+		return nil, errors.New("No OSIN_DB_HOST env is provided!")
+	}
+
+	osinDbPort, ok := os.LookupEnv("OSIN_DB_PORT")
+	if !ok || len(osinDbPort) == 0 {
+		log.Warn("No OSIN_DB_PORT env is provided and default value 3306 is used.")
+		osinDbPort = "3306"
+	}
+
+	osinDbDatabse, ok := os.LookupEnv("OSIN_DB_DATABASE")
+	if !ok || len(osinDbDatabse) == 0 {
+		return nil, errors.New("No OSIN_DB_DATABASE env is provided!")
+	}
+
+	db, err := sql.Open("mysql", fmt.Sprintf("%v:%v@tcp(%v:%v)/%v?parseTime=true",
+		osinDbUsername,
+		osinDbPassword,
+		osinDbHost,
+		osinDbPort,
+		osinDbDatabse,
+	))
 	if err != nil {
 		return nil, err
 	}
 
-	store := mysql.New(db,"osin_")
+	osinTablePrefix, ok := os.LookupEnv("OSIN_TABLE_PREFIX")
+	if !ok || len(osinTablePrefix) == 0 {
+		log.Warn("No OSIN_TABLE_PREFIX env is provided and default value \"osin_\" is used.")
+		osinTablePrefix = "osin_"
+	}
+
+	store := mysql.New(db, osinTablePrefix)
 	if err := store.CreateSchemas(); err != nil {
 		log.Warnf("Error creating osin schemas: %v", err.Error())
 	}
